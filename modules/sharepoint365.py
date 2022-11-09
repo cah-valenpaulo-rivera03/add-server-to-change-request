cbd331fdf4f8d9cdc486b5e73d04fdd9c6e9af41
cah-valenpaulo-rivera03 <valenpaulo.rivera@cardinalhealth.com>
2022-11-09 05:36:29 -0500

    Catch python SSL error in connecting to Sharepoint

--- modules/sharepoint365.py
+++ modules/sharepoint365.py
@@ -1,5 +1,9 @@
 import os
+import time
 import dotenv
+import requests
+
+from _ssl import SSLCertVerificationError
 
 from config.settings import *
 
@@ -25,14 +29,64 @@ class Sharepoint365:
         with open(destination_path, "wb") as target_file:
             file = conn.web.get_file_by_server_relative_url(file_url)
             file.download(target_file)
-            conn.execute_query()
+            
+            while True:
+                if catch_download_ssl_error(conn):
+                    break
+                else:
+                    continue
 
     def list_files(self, source_directory):
         conn = self.auth()
         directory = ctx.web.get_folder_by_server_relative_url(source_directory)
         files = directory.files
-        conn.load(files)
-        conn.execute_query()
+        
+        while True:
+            if catch_list_ssl_error(conn, files):
+                break
+            else:
+                continue
 
         return files
+
+    def catch_download_ssl_error(conn):
+        try:
+            conn.execute_query()
+            return True
+        except SSLCertVerificationError:
+            time.sleep(1)
+            print("Reconnecting...")
+            return False
+
+        except requests.exceptions.SSLError:
+            time.sleep(1)
+            print("Reconnecting...")
+            return False
+
+        except AttributeError:
+            time.sleep(1)
+            print("Reconnecting...")
+            return False
+
+    def catch_list_ssl_error(conn, files):
+        try:
+            conn.load(files)
+            conn.execute_query()
+            return True
+        except SSLCertVerificationError:
+            time.sleep(1)
+            print("Reconnecting...")
+            return False
+
+        except requests.exceptions.SSLError:
+            time.sleep(1)
+            print("Reconnecting...")
+            return False
+
+        except AttributeError:
+            time.sleep(1)
+            print("Reconnecting...")
+            return False
+
+        
         
